8e3baec8bbc56fc38e4c1d25aa90632b74030879
lovelysusie <root@HaonandeMacBook-Pro.local>
2016-11-21 16:51:35 +0800

    add changes

--- band_pass.py
+++ band_pass.py
@@ -36,21 +36,17 @@ lowcut = 0.2
 highcut = 4
 
 Fs = 67*2
-#sensorData = pd.read_csv("/Volumes/HAONAN/1.raw data/PT17.0105_M.csv",low_memory=True)
-#accelerX = sensorData['accelerometerX']
-#accelerX = accelerX[59:7562]
-#y = accelerX
+
 y = np.array([0.0888672,0.0961914,0.122314,0.134033,0.14502,0.14502,0.140137,0.134033,0.118408,0.105469,0.0905762,0.0947266,0.0986328,0.118896,0.136963,0.14209,0.140381,0.122559,0.119629,0.111328,0.100342,0.0908203,0.0922852,0.111084,0.139648,0.148193,0.151611,0.141113,0.131836,0.12207,0.107666,0.104004,0.0998535,0.10083,0.104248,0.116943,0.124756,0.125244,0.123779,0.120605,0.118652,0.111572,0.117676,0.121094,0.134766,0.145264,0.139893,0.128174,0.112305,0.11084,0.107666,0.104492,0.102051,0.0939941,0.101074,0.107422,0.131104,0.156006,0.161133,0.158691,0.145508,0.137695,0.117188,0.0974121,0.088623,0.0856934])
 
 f1,accx = signal.periodogram(y,fs=Fs,scaling='density',return_onesided=True)
 
 ax = butterBandPassFilter(y,lowcut,highcut,Fs,order=6)
+
+# drawing the periodogram
 f,gyX = signal.periodogram(ax,fs=Fs,scaling='density',return_onesided=True)
 plt.subplot(211)
 plt.plot(f1,accx)
 plt.subplot(212)
 plt.plot(f,gyX)
-accx = accx[1:223]
-print accx.mean()
-print accx.max()
 plt.show()
