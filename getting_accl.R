1d05879c76f7e3be7ebc3c5f2ba2f7a9d7cc1f28
lovelysusie <haonanshihaoren@126.com>
2016-11-23 14:42:40 +0800

    made changes

--- getting_accl.R
+++ getting_accl.R
@@ -1,45 +1,46 @@
 library(dplyr)
 library(data.table)
 library(psd)
+library(signal)
+library(ggplot2)
 
-
-senser <-read.csv(file.choose())
+sensor <-read.csv(file.choose())
 
 #delet unused columns
-senser <-senser[,-(9:12)]
-head(senser)
-table(senser$events)
+sensor <-sensor[,-(9:12)]
+head(sensor)
+table(sensor$events)
 
-senser<-data.frame(senser)
-senser$accl <-sqrt(senser$accelerometerX^2+senser$accelerometerY^2+senser$accelerometerZ^2)
-head(senser$accl)
+sensor<-data.frame(sensor)
+sensor$accl <-sqrt(sensor$accelerometerX^2+sensor$accelerometerY^2+sensor$accelerometerZ^2)
+head(sensor$accl)
 
 ######################
 #without drift cancelation
-acceleration <-aggregate(senser$accl,list(senser$ReadableTime), max)
+acceleration <-aggregate(sensor$accl,list(sensor$ReadableTime), max)
 
-matching <-data.frame(senser$ReadableTime,senser$PD3)
-matching <-matching[!duplicated(matching$senser.ReadableTime), ]
+matching <-data.frame(sensor$ReadableTime,sensor$PD3)
+matching <-matching[!duplicated(matching$sensor.ReadableTime), ]
 acceleration <-cbind(acceleration,matching)
 acceleration <-acceleration[,-3]
 names(acceleration)<-c("time","max acceleration","PD3")
 #############
-table(senser$symptoms)
-matching <-data.frame(senser$ReadableTime,senser$symptoms)
-matching <-matching[!duplicated(matching$senser.ReadableTime), ]
+table(sensor$symptoms)
+matching <-data.frame(sensor$ReadableTime,sensor$symptoms)
+matching <-matching[!duplicated(matching$sensor.ReadableTime), ]
 #matching <-matching[-341,]
 acceleration <-cbind(acceleration,matching)
 acceleration <-acceleration[,-4]
 names(acceleration)[4]<-"main symptons"
 #############
-matching <-aggregate(senser$accl,list(senser$ReadableTime), mean)
+matching <-aggregate(sensor$accl,list(sensor$ReadableTime), mean)
 acceleration <-cbind(acceleration,matching)
 acceleration <-acceleration[,-5]
 names(acceleration)[5]<-"mean acceleration"
 #############
 i=1
-series <-filter(senser,senser$ReadableTime==acceleration$time[i]|
-                  senser$ReadableTime==acceleration$time[i+1])
+series <-filter(sensor,sensor$ReadableTime==acceleration$time[i]|
+                  sensor$ReadableTime==acceleration$time[i+1])
 series <-data.frame(mean(series$accl),max(series$accl))
 series1 <-series
 
@@ -47,8 +48,8 @@ series1 <-series
 i=3
 j=nrow(acceleration)+1
 while (i<j) {
-  series <-filter(senser,senser$ReadableTime==acceleration$time[i]|
-                    senser$ReadableTime==acceleration$time[i+1])
+  series <-filter(sensor,sensor$ReadableTime==acceleration$time[i]|
+                    sensor$ReadableTime==acceleration$time[i+1])
   series <-data.frame(mean(series$accl),max(series$accl))
   series1 <-rbind(series1,series)
   i=i+2
@@ -63,24 +64,29 @@ names(series1)[1:2] <-c("mean", "max")
 #######
 names(acceleration) <-c("time","accl")
 
+bandpassfilter <-butter(n=6, W=c(0.2/62,4/62),type="pass")
+
 fun1 <-function(x) {
-  x <-pspectrum(x$accelerometerX, x.frqsamp=62*2, niter = 1)
-  x <-data.frame(freq=x$freq,spec=x$spec)
-  x <-filter(x, x$freq>0.2 & x$freq<4)
+  x =filter(bandpassfilter, x$accelerometerX)
+  x =c(x)
+  x =pspectrum(x, x.frqsamp=62*2, niter = 1)
+  x =data.frame(freq=x$freq,spec=x$spec)
+  x =data.table(x)
+  x =x[freq>0.2 & freq<4]
   x <-mean(x$spec)
 }
 
 i=1
-df <-filter(senser,senser$ReadableTime==acceleration$time[i]|
-              senser$ReadableTime==acceleration$time[i+1])
+df <-filter(sensor,sensor$ReadableTime==acceleration$time[i]|
+              sensor$ReadableTime==acceleration$time[i+1])
 df <-data.frame(MSP=fun1(df))
 df1 <-df
 #######
 i=3
 j=nrow(acceleration)+1
 while (i<j) {
-  df <-filter(senser,senser$ReadableTime==acceleration$time[i]|
-                senser$ReadableTime==acceleration$time[i+1])
+  df <-filter(sensor,sensor$ReadableTime==acceleration$time[i]|
+                sensor$ReadableTime==acceleration$time[i+1])
   df <-data.frame(MSP=fun1(df))
   df1 <-rbind(df1,df)
   i=i+2
@@ -93,6 +99,7 @@ series1 <-series1[,-2]
 healthy <-read.csv(file.choose())
 mean(healthy$MSP)
 mean(series1$MSP)
+
 ##########################
 setwd("/Users/Susie/Desktop")
 write.csv(series1, "PT.0007_MSP_b.csv", row.names = FALSE)
