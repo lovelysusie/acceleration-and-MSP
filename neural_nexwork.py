967c6ba3ccc780528a5d096e49e82d80c68763ef
lovelysusie <haonanshihaoren@126.com>
2016-11-30 16:08:44 +0800

    made changes

--- neural_nexwork.py
+++ neural_nexwork.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 X = np.array(([3,5],[5,1],[10,2]) , dtype = float)
 y = np.array(([75],[82],[93]) , dtype = float)
 X = X/np.amax(X, axis = 0)
@@ -13,10 +14,8 @@ testInput = np.arange(-6,6,0.1)
 plt.plot(testInput, sigmoid(testInput), linewidth = 2)
 plt.show()
 
-print sigmoid(1)
-
 class Neural_network(object):
-    def _init_(self, activication = "tanh"):
+    def _init_(self):
         self.inputLayerSize = 2
         self.outputLayerSize = 1
         self.hiddenLayerSize = 3
@@ -39,3 +38,31 @@ class Neural_network(object):
         return 1/(1+np.exp(-z))
 NN = Neural_network()
 yHat = NN.forward(X)
+
+weightsToTry = np.linspace(-10, 10, 1000)
+costs = np.zeros(1000)
+costs[i] = 0.5*sum((y-yHat)**2)
+
+def sigmoidPrime(z):
+    #Deriviative of Sigmoid Function
+    return np.exp(-z)/((1+np.exp(-z))**2)
+
+def costFunctionPrime(self, X, y):
+    self.yHat = self.forward(X)
+
+    delta3 = np.multiply(-(y-self.yHat), self.sigmoidPrime(self.z3))
+    dJdW2 = np.dot(self.a2.T, delta3)
+
+    delta2 = np.dot(delta3, self.W2.T)*self.sigmoidPrime(self.z2)
+    dJdW1 = np.dot(X.T, delta2)
+
+    return dJdW1, dJdW2
+
+NN = Neural_network()
+cost1 = NN.costFunction(X,y)
+
+dJdW1, dJdW2 = NN.costFunctionPrime(X,y)
+NN.W1 = NN.W1 - scalar*dJdW1
+NN.W2 = NN.W1 - scalar*dJdW1
+cost3 = NN.costFunction(X,y)
+print cost3 ,cost2
