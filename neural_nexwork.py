330124c2a5ca82e8aed051b2b1055abb700d59a1
lovelysusie <haonanshihaoren@126.com>
2016-12-01 13:43:35 +0800

    made changes

--- neural_nexwork.py
+++ neural_nexwork.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import optimize
 
 
 X = np.array(([3,5],[5,1],[10,2]) , dtype = float)
@@ -14,55 +15,184 @@ testInput = np.arange(-6,6,0.1)
 plt.plot(testInput, sigmoid(testInput), linewidth = 2)
 plt.show()
 
-class Neural_network(object):
-    def _init_(self):
+class Neural_Network(object):
+    def __init__(self):
+        #Define Hyperparameters
         self.inputLayerSize = 2
         self.outputLayerSize = 1
         self.hiddenLayerSize = 3
 
-        # setting the weight
-        self.W1 = np.random.randn(self.inputLayerSize, \
-                                self.hiddenLayerSize)
-        self.W2 = np.random.randn(self.hiddenLayerSize, \
-                                self.outputLayerSize)
+        #Weights (parameters)
+        self.W1 = np.random.randn(self.inputLayerSize,self.hiddenLayerSize)
+        self.W2 = np.random.randn(self.hiddenLayerSize,self.outputLayerSize)
 
     def forward(self, X):
-        #propagate inputs through network
-        self.z2 = np.dot(X,self.W1)
+        #Propogate inputs though network
+        self.z2 = np.dot(X, self.W1)
         self.a2 = self.sigmoid(self.z2)
         self.z3 = np.dot(self.a2, self.W2)
         yHat = self.sigmoid(self.z3)
         return yHat
+
     def sigmoid(self, z):
-        #Apply sigmoid actvation funtion to scalar, vector or array
+        #Apply sigmoid activation function to scalar, vector, or matrix
         return 1/(1+np.exp(-z))
-NN = Neural_network()
-yHat = NN.forward(X)
 
-weightsToTry = np.linspace(-10, 10, 1000)
-costs = np.zeros(1000)
-costs[i] = 0.5*sum((y-yHat)**2)
+    def sigmoidPrime(self,z):
+        #Gradient of sigmoid
+        return np.exp(-z)/((1+np.exp(-z))**2)
 
-def sigmoidPrime(z):
-    #Deriviative of Sigmoid Function
-    return np.exp(-z)/((1+np.exp(-z))**2)
+    def costFunction(self, X, y):
+        #Compute cost for given X,y, use weights already stored in class.
+        self.yHat = self.forward(X)
+        J = 0.5*sum((y-self.yHat)**2)
+        return J
 
-def costFunctionPrime(self, X, y):
-    self.yHat = self.forward(X)
+    def costFunctionPrime(self, X, y):
+        #Compute derivative with respect to W and W2 for a given X and y:
+        self.yHat = self.forward(X)
 
-    delta3 = np.multiply(-(y-self.yHat), self.sigmoidPrime(self.z3))
-    dJdW2 = np.dot(self.a2.T, delta3)
+        delta3 = np.multiply(-(y-self.yHat), self.sigmoidPrime(self.z3))
+        dJdW2 = np.dot(self.a2.T, delta3)
 
-    delta2 = np.dot(delta3, self.W2.T)*self.sigmoidPrime(self.z2)
-    dJdW1 = np.dot(X.T, delta2)
+        delta2 = np.dot(delta3, self.W2.T)*self.sigmoidPrime(self.z2)
+        dJdW1 = np.dot(X.T, delta2)
 
-    return dJdW1, dJdW2
+        return dJdW1, dJdW2
 
-NN = Neural_network()
-cost1 = NN.costFunction(X,y)
+    #Helper Functions for interacting with other classes:
+    def getParams(self):
+        #Get W1 and W2 unrolled into vector:
+        params = np.concatenate((self.W1.ravel(), self.W2.ravel()))
+        return params
 
-dJdW1, dJdW2 = NN.costFunctionPrime(X,y)
-NN.W1 = NN.W1 - scalar*dJdW1
-NN.W2 = NN.W1 - scalar*dJdW1
-cost3 = NN.costFunction(X,y)
-print cost3 ,cost2
+    def setParams(self, params):
+        #Set W1 and W2 using single paramater vector.
+        W1_start = 0
+        W1_end = self.hiddenLayerSize * self.inputLayerSize
+        self.W1 = np.reshape(params[W1_start:W1_end], (self.inputLayerSize , self.hiddenLayerSize))
+        W2_end = W1_end + self.hiddenLayerSize*self.outputLayerSize
+        self.W2 = np.reshape(params[W1_end:W2_end], (self.hiddenLayerSize, self.outputLayerSize))
+
+    def computeGradients(self, X, y):
+        dJdW1, dJdW2 = self.costFunctionPrime(X, y)
+        return np.concatenate((dJdW1.ravel(), dJdW2.ravel()))
+
+
+
+def computeNumericalGradient(N, X, y):
+        paramsInitial = N.getParams()
+        numgrad = np.zeros(paramsInitial.shape)
+        perturb = np.zeros(paramsInitial.shape)
+        e = 1e-4
+
+        for p in range(len(paramsInitial)):
+            #Set perturbation vector
+            perturb[p] = e
+            N.setParams(paramsInitial + perturb)
+            loss2 = N.costFunction(X, y)
+
+            N.setParams(paramsInitial - perturb)
+            loss1 = N.costFunction(X, y)
+
+            #Compute Numerical Gradient
+            numgrad[p] = (loss2 - loss1) / (2*e)
+
+            #Return the value we changed to zero:
+            perturb[p] = 0
+
+        #Return Params to original value:
+        N.setParams(paramsInitial)
+
+        return numgrad
+
+
+class trainer(object):
+    def __init__(self, N):
+        #Make Local reference to network:
+        self.N = N
+
+    def callbackF(self, params):
+        self.N.setParams(params)
+        self.J.append(self.N.costFunction(self.X, self.y))
+
+    def costFunctionWrapper(self, params, X, y):
+        self.N.setParams(params)
+        cost = self.N.costFunction(X, y)
+        grad = self.N.computeGradients(X,y)
+
+        return cost, grad
+
+    def train(self, X, y):
+        #Make an internal variable for the callback function:
+        self.X = X
+        self.y = y
+
+        #Make empty list to store costs:
+        self.J = []
+
+        params0 = self.N.getParams()
+
+        options = {'maxiter': 200, 'disp' : True}
+        _res = optimize.minimize(self.costFunctionWrapper, params0, jac=True, method='BFGS', \
+                                 args=(X, y), options=options, callback=self.callbackF)
+
+        self.N.setParams(_res.x)
+        self.optimizationResults = _res
+
+NN = Neural_Network()
+T = trainer(NN)
+T.train(X,y)
+
+plot(T.J)
+grid(1)
+xlabel('Iterations')
+ylabel('Cost')
+
+NN.costFunctionPrime(X,y)
+
+NN.forward(X)
+
+print y
+
+#Test network for various combinations of sleep/study:
+hoursSleep = linspace(0, 10, 100)
+hoursStudy = linspace(0, 5, 100)
+
+#Normalize data (same way training data way normalized)
+hoursSleepNorm = hoursSleep/10.
+hoursStudyNorm = hoursStudy/5.
+
+#Create 2-d versions of input for plotting
+a, b  = meshgrid(hoursSleepNorm, hoursStudyNorm)
+
+#Join into a single input matrix:
+allInputs = np.zeros((a.size, 2))
+allInputs[:, 0] = a.ravel()
+allInputs[:, 1] = b.ravel()
+allOutputs = NN.forward(allInputs)
+
+#Contour Plot:
+yy = np.dot(hoursStudy.reshape(100,1), np.ones((1,100)))
+xx = np.dot(hoursSleep.reshape(100,1), np.ones((1,100))).T
+
+CS = contour(xx,yy,100*allOutputs.reshape(100, 100))
+clabel(CS, inline=1, fontsize=10)
+xlabel('Hours Sleep')
+ylabel('Hours Study')
+
+#3D plot:
+
+##Uncomment to plot out-of-notebook (you'll be able to rotate)
+#%matplotlib qt
+
+from mpl_toolkits.mplot3d import Axes3D
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+
+surf = ax.plot_surface(xx, yy, 100*allOutputs.reshape(100, 100), \
+                       cmap=cm.jet)
+
+ax.set_xlabel('Hours Sleep')
+ax.set_ylabel('Hours Study')
+ax.set_zlabel('Test Score')
