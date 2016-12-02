a632af0d095fbdc4bae74fa9173785f230fc1c55
lovelysusie <haonanshihaoren@126.com>
2016-12-02 13:25:52 +0800

    made changes

--- NN_work.py
+++ NN_work.py
@@ -1,11 +1,13 @@
 from sklearn.neural_network import MLPClassifier
 import numpy as np
+import matplotlib.pyplot as plt
+import array
 
 
-X = np.genfromtxt('PT0026_M_x.csv',delimiter=',',dtype=None, names=True)
+X = np.genfromtxt('train_x.csv',delimiter=',',dtype=None, names=True)
 X = np.array(X).tolist()
 
-y = np.genfromtxt('PT0026_M_y.csv',delimiter=',',dtype=None, names=True)
+y = np.genfromtxt('train_y.csv',delimiter=',',dtype=None, names=True)
 y = np.array(y).tolist()
 
 
@@ -14,5 +16,22 @@ clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
 
 clf.fit(X, y)
 
-print(clf.predict([[2., 2.,1.5,3.5, 2.5,0.8], [2., 2.,1.5,3.5, 2.5,0.8]]))
+test_X = np.genfromtxt('test_x.csv',delimiter=',',dtype=None, names=True)
+test_X = np.array(test_X).tolist()
 
+test_y = np.genfromtxt('test_y.csv',delimiter=',',dtype=None, names=True)
+# test_y = np.array(test_y).tolist()
+
+
+result_y = clf.predict(test_X)
+result_y = np.asarray(result_y)
+print(len(result_y))
+print(result_y.__len__())
+xaxis = np.arange(1,50001,1)
+print(xaxis.__len__())
+
+plt.plot(np.arange(1,50001,1), result_y, linewidth = 2)
+plt.show()
+
+plt.plot(np.arange(1,50001,1), test_y, linewidth = 2)
+plt.show()
