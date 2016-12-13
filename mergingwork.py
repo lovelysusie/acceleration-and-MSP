4146bda695a62959499f2228924b163b5538a6ab
lovelysusie <haonanshihaoren@126.com>
2016-12-13 12:11:26 +0800

    made changes

--- mergingwork.py
+++ mergingwork.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import numpy as np
+# if the file is 12HRS, run the code below:
 
+'''
 sensorData = pd.read_csv("/Users/Susie/Documents/patientdata0016/PT17.0016.csv",low_memory=True)
 # loading data
 sensorData['ReadableTime'] = pd.to_datetime(sensorData['time'], unit='ms')
@@ -16,3 +19,27 @@ result = pd.merge(sensorData, clinicalData, how='left', on=['ReadableTime'])
 print(result.head(5))
 
 result.to_csv('PT17.0016_M.csv', index=False)
+'''
+
+# if the file is 15 min, run the code below:
+sensorData = pd.read_csv("/Users/Susie/Documents/PT17.0105/PT17.0105_01.csv",low_memory=True)
+sensorData['timeInSecond'] = sensorData['time']/1000
+
+clinicalData = pd.read_excel("/Users/Susie/Documents/PT17.0105/PT17.0105.xlsx", sheetname=1,header=0,
+                             skiprows=5)
+clinicalData = clinicalData[2:]
+
+dataFrame = sensorData[(sensorData['timeInSecond'] > clinicalData.at[2, 'UNIX-Time']) &
+           (sensorData['timeInSecond'] < clinicalData.at[3, 'UNIX-Time'])]
+dataFrame['timeGroup'] = clinicalData.at[2, 'UNIX-Time']
+j = dataFrame.__len__()
+sensorData.at[1:j,'timeInSecond'] = dataFrame['timeGroup']
+
+for time in clinicalData['UNIX-Time']:
+    
+
+
+
+
+
+print(sensorData.head(5))
